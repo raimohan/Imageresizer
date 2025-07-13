db496b43e98b3b5b21afdf8fa3b03d179ca3532c
raimohan <raimohan9676@gmail.com>
2025-07-13 10:12:51 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,31 @@
 // next.config.ts
 
-import type { NextConfig } from 'next'
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-}
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'iili.io', // ✅ For your logo hosted at https://iili.io/FM85HeS.png
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
 
-export default nextConfig
+export default nextConfig;
